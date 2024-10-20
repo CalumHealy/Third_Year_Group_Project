f5f55be8a34b550f2668184c3b6f26633c7f5ab9
hom03 <hughdomahony@gmail.com>
2024-10-20 23:38:22 +0100

    login functional and more pages added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 import router from "./routes/routing.js";
 
 dotenv.config();//load environment vars from .env
@@ -26,9 +27,10 @@ const firebaseConfig = {
     databaseURL: process.env.FIREBASE_DB_URL
 };
 
-//init firebase app & db
+//init firebase app,db,auth
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getDatabase(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 //middleware 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -46,4 +48,4 @@ app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)//log url when server boots
 })
 
-export { db };
+export { db,auth };
--- routes/routing.js
+++ routes/routing.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { db } from '../app.js';
-import { ref,set } from 'firebase/database';
+import { ref,set,get } from 'firebase/database';
+import {getAuth, sendPasswordResetEmail} from 'firebase/auth'
 import argon2  from 'argon2'; //for password hashing
 
 const router = express.Router();
@@ -24,7 +25,7 @@ const restoreEmail = (safeEmail) => {
     return safeEmail.replace(/_at_/g,'@').replace(/_dot_/g,'.');
 };
 
-//Home route
+//Initial login route
 router.get('/',(req,res) => {
     res.render('index'); //render login.ejs
 });
@@ -65,6 +66,97 @@ router.post('/register',async (req,res) =>{
 //Forgot Password route
 router.get('/forgotPassword',(req,res) =>{
     res.render('forgotPass'); //render forgotPass.ejs 
+});
+
+//Forgot Password email send
+router.post('/forgotPassword',async (req,res) =>{
+    const { email } = req.body;
+    console.log(email);
+    const auth = getAuth();
+
+    try{
+        await sendPasswordResetEmail(auth,email)
+        .then(() =>{
+            console.log(`Password reset email sent to ${email}`);
+            res.send("Password reset email sent successfully");
+        })
+        .catch((error) =>{
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error(`ErrorCode: ${errorCode},ErrorMessage: ${errorMessage}`);
+            res.status(405).send("Error sending password reset email.")
+        });
+    }catch(error){
+        console.error("Error sending reset email: ",error);
+        res.status(500).send("Error trying to send password reset email");
+    }
+});
+
+//Home route
+router.get('/home', (req,res) => {
+    res.render('home'); //render home.ejs
+});
+
+//login retrieval
+router.post('/login', async (req,res) =>{
+    const { username, password} = req.body;
+    
+    if(!username || !password){
+        return res.status(400).send("Username and password are required.")
+    }
+
+    try{
+        //create reference to user data in firebase
+        const safeUsername = safeEmail(username);
+        const userRef = ref(db, 'users/' + safeUsername);
+
+        //get user data
+        const snapshot = await get(userRef);
+
+        if (!snapshot.exists()){
+            return res.status(400).send("User not found.");
+        }
+
+        const userData = snapshot.val();
+
+        //verify password
+        const isPassword = await argon2.verify(userData.password,password);
+        if (!isPassword){
+            return res.status(401).send("Invalid password.");
+        }
+
+        console.log("User logged in successfully.");
+        res.redirect('/home'); //if login is a success then send the fund manager through to the home screen
+
+    }catch(error){
+        console.error("Error logging into website",error);
+        res.status("402").send("Failed to login.");
+    }
+});
+
+// stock invest route
+router.get('/buystock',async (req,res)=>{
+    res.render('buystock'); //renders buystock.ejs
+});
+
+// crypto invest route
+router.get('/buycrypto',async (req,res)=>{
+    res.render('buycrypto'); //renders buycrypto.ejs
+});
+
+// help route
+router.get('/help',async (req,res)=>{
+    res.render('help'); //renders help.ejs
+});
+
+// settings route
+router.get('/settings',async (req,res)=>{
+    res.render('settings'); //renders setting.ejs
+});
+
+// review route
+router.get('/review',async (req,res)=>{
+    res.render('review'); //renders review.ejs
 });
 
 export default router;
